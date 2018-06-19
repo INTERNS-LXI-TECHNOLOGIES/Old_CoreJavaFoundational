99bd669bf64ff26d8bc2829c8c0cc4f937672c7a
sreerajthampan <sreeraj.thampan@lxisoft.com>
2018-06-19 16:57:47 +0530

    trial

--- animalGame v1.2/TDD.java
+++ animalGame v1.2/TDD.java
@@ -3,10 +3,10 @@ class TDD{
 	public static void main(String args[])
 	{
 		
-		System.out.println("**************************Animal GAME********************************\n");
+		System.out.println("**************************Animal GAME**************************************\n");
 		Forest f=new Forest(6,5,4);
 		
-		System.out.println("**************************SURVIVAL Result********************************\n");
+		System.out.println("**************************SURVIVAL Result**************************************\n");
 		
 		f.outputGrid();
 		f.animalSurvived();
