380c9b4939a9cf03a2a09259011fa4f3eb150498
balagopal890 <balagopal.v@lxisoft.com>
2018-06-22 04:21:31 -0700

    Implementing exception

---
+++ AnimalGamev1.5/com/lxisoft/animalgame/animal/Tiger.java
@@ -0,0 +1,27 @@
+package com.lxisoft.animalgame.animal;
+import com.lxisoft.animalgame.interfaces.*;
+import com.lxisoft.animalgame.forest.*;
+import java.io.*;
+import java.util.*;
+public class Tiger extends Animal implements Carnivorus{
+	//setting all the values
+	public Tiger(int id){
+	       Random r=new Random();
+		   setName("Tiger"+String.valueOf(id));
+		   setStrength(r.nextInt(10));
+		   setReach(r.nextInt(10));
+		   setHungry(r.nextInt(10));
+	       setCarnivorus(1);
+	}
+    public void fight(Animal f,Animal c){
+			System.out.println(c.getName()+" figths with  "+f.getName());  
+		   }
+    public void eats(Animal f,Animal c){
+		System.out.println(c.getName()+" eats  "+f.getName());
+	}	   
+	public void killed(Animal f,Animal c){
+		System.out.println(c.getName()+" has been killed by  "+f.getName());
+	}
+    
+	
+}
---
+++ AnimalGamev1.5/com/lxisoft/animalgame/test/TDD.java
@@ -0,0 +1,19 @@
+package com.lxisoft.animalgame.test;
+import com.lxisoft.animalgame.forest.Forest;
+import com.lxisoft.animalgame.exceptions.*;
+import java.io.*;
+
+public class TDD{
+	
+	public static void main(String args[])throws DeadException
+	{
+	System.out.println("**********************MY SURVIVAL GAME***********************\n");
+        System.out.println("\n\n");
+	System.out.println("\n\n********POSITION OF THE ANIMALS IN THE FOREST************\n");
+	Forest f=new Forest(6,3,5,4,5);
+	System.out.println("\n\n********SURVIVAL STATUS OF THE ANIMALS*******************\n");
+	f.outGrid();
+	f.finalStatus();	
+	}
+	
+    }
