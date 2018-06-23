67647d6f91c8a6a5d774ade4092ac98e917897ac
balagopal890 <balagopal.v@lxisoft.com>
2018-06-23 03:26:03 -0700

    implementing Try and Catch

---
+++ AnimalGamev1.6/com/lxisoft/animalgame/animal/Deer.java
@@ -0,0 +1,21 @@
+package com.lxisoft.animalgame.animal;
+import com.lxisoft.animalgame.interfaces.*;
+import com.lxisoft.animalgame.forest.*;
+import java.io.*;
+import java.util.*;
+public class Deer extends Animal implements Herbivorus{
+	//setting all the values
+public Deer(int id){
+	       Random r=new Random();
+		   setName("Deer "+String.valueOf(id));
+		   setStrength(r.nextInt(10));
+		   setReach(r.nextInt(10));
+		   setHungry(r.nextInt(10));
+		   setLuck(r.nextInt(10));
+		   setCarnivorus(0);
+}
+public void runaway(Animal b){
+	System.out.println(b.getName()+"Runsaway dueto its Luck");
+}
+   
+}
