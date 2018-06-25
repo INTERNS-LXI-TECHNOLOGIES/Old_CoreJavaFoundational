712861a2c10746265ecc0de81a228338be3f4aab
balagopal890 <balagopal.v@lxisoft.com>
2018-06-24 20:57:25 -0700

    implemented userdefined exception

---
+++ AnimalGamev1.7/com/lxisoft/animalgame/animal/Zebra.java
@@ -0,0 +1,20 @@
+package com.lxisoft.animalgame.animal;
+import com.lxisoft.animalgame.interfaces.*;
+import com.lxisoft.animalgame.forest.*;
+import java.io.*;
+import java.util.*;
+public class Zebra extends Animal implements Herbivorus{
+	//setting all the values
+	public Zebra(int id){
+		   Random r=new Random();
+		   setName("Zebra "+String.valueOf(id));
+		   setStrength(r.nextInt(10));
+		   setReach(r.nextInt(10));
+		   setHungry(r.nextInt(10));
+		   setLuck(r.nextInt(10));
+		   setCarnivorus(0);
+		   }
+	public void runaway(Animal b){
+	System.out.println(b.getName()+" Runsaway dueto its Luck");
+}
+	}
