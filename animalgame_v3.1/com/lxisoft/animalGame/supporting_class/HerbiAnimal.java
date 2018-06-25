6e854af3505ec841d9dd243f4d78d670f9a8673c
sreerajthampan <sreeraj.thampan@lxisoft.com>
2018-06-25 15:58:52 +0530

    user defined error handler

--- animalgame_v3.1/com/lxisoft/animalGame/supporting_class/HerbiAnimal.java
+++ animalgame_v3.1/com/lxisoft/animalGame/supporting_class/HerbiAnimal.java
@@ -1,10 +1,11 @@
 package com.lxisoft.animalGame.supporting_class;
 import com.lxisoft.animalGame.supporting_class.*;
+import com.lxisoft.animalGame.exception.*;
 import com.lxisoft.animalGame.animal.*;
 public class HerbiAnimal extends Animal implements Herbivorous{
-		public void eatGrass(Animal d)
-	{try{
-	System.out.println(d.getName()+" is eating grass");}catch(NullPointerException e){System.out.printerr();}
+		public void eatGrass(Animal d)throws NullException
+	{
+	System.out.println(d.getName()+" is eating grass");
 	}
 	
 	
