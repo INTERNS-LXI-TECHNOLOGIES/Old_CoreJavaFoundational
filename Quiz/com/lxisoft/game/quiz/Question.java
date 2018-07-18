a0bcd3082b1f5c7e04297e5d8a8d2250f87b9b7f
sumig1 <sumi.g@lxisoft.com>
2018-07-18 16:28:32 +0530

    task completed

--- Quiz/com/lxisoft/game/quiz/Question.java
+++ Quiz/com/lxisoft/game/quiz/Question.java
@@ -11,12 +11,11 @@ public class Question
 	public static void questionOption()
 	{
 		
-		String question="",option="",answer="";
-		System.out.println("GAME STARTING");
+		String question="",option="",answer="",answer1="";
+		System.out.println("GAME STARTING\n");
 		String file="D:/workspace/Team1/Quiz/question.txt";
-		for(int ln=1;ln<=10;ln++)
-		{
-			System.out.println(ln+":");
+		int mark=0;
+			
 			try
 			{
 				FileReader fr=new FileReader(file);
@@ -24,8 +23,11 @@ public class Question
 				String line;
 				char qu;
 				int i=0;
+				int ln=1;
 				while((line=br.readLine())!=null)
 				{
+					
+					System.out.print(ln+":");
 					i=0;
 					while((qu=line.charAt(i))!=';')
 					{
@@ -34,7 +36,7 @@ public class Question
 						
 					}
 					System.out.println(question);
-					
+					System.out.println();
 					i++;
 					while((qu=line.charAt(i))!=';')
 					{
@@ -42,20 +44,34 @@ public class Question
 						i++;
 						
 					}
+					i++;
 					System.out.println(option);
-					
+					System.out.println();
 					System.out.println("Enter your option");
 					Scanner input=new Scanner(System.in);
 					answer=input.nextLine();
-					
+					while((qu=line.charAt(i))!=';')
+					{
+						answer1+=qu;
+						i++;
+						
+					}
+					if(answer.equals(answer1))
+					{
+						mark++;
+					}
+					question="";option="";answer1="";answer="";
+					ln++;
 					
 				
 				}
+			System.out.println("Your Score is: "+mark+" Out of 5");
 			}
+	
 			catch(IOException e)
 			{
 				System.out.println("IOException");
 			}
-		}
+		
 	}
 }
