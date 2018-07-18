b3555e6cedbb91a3b40029e5c414d47fc2b70291
athulya <athulya.somakumaran@lxisoft.com>
2018-07-18 16:04:25 +0530

    inheritance1

---
+++ inheritance1/test/TDD.java
@@ -0,0 +1,15 @@
+package com.lxisoft.inheritance.test;
+import com.lxisoft.inheritance.name.Name;
+
+public class TDD
+{
+public static void main(String args[])
+{
+Name n=new Name();
+
+n.setname("athu");
+String x=n.getname();
+System.out.println(x);
+
+}
+}
