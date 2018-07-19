75fcd61e9294dc525ff4cf40956ed8ea74f14831
AryaVineesh <arya.p@lxisoft.com>
2018-07-19 16:52:32 +0530

    version 1.1

---
+++ college_management_v1.1/com/lxisoft/college/student/MECH.java
@@ -0,0 +1,11 @@
+package com.lxisoft.college.student;
+
+public class MECH extends Student
+{
+	
+	public void print_mech_student_details(String Stud)
+	{
+		
+		System.out.println("Student"+" "+Stud+" "+"is a mechanic");
+	}	
+}
