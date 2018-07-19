c1e508d81aaddd249da085875d6bb2861da26ff7
athulya <athulya.somakumaran@lxisoft.com>
2018-07-19 12:50:39 +0530

    inheritance concept

---
+++ inheritance2/student/Student.java
@@ -0,0 +1,14 @@
+package com.lxisoft.inheritance2.student;
+
+public class Student
+{
+public String name;
+public String address;
+
+public void display1()
+{
+
+System.out.println("name" +name);
+System.out.println("address" +address);
+}
+}
---
+++ inheritance2/student/dept/CSE.java
@@ -0,0 +1,10 @@
+package com.lxisoft.inheritance2.student.dept;
+import com.lxisoft.inheritance2.student.Student;
+
+public class CSE extends Student
+{
+public void show()
+{
+System.out.println("We are programmers");
+}
+}
