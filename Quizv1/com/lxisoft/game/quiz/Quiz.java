45cc51b17bfc0b75ff9bdf861c06518ca426473e
sumig1 <sumi.g@lxisoft.com>
2018-07-19 15:55:50 +0530

    modified the quiz project

---
+++ Quizv1/com/lxisoft/game/quiz/Quiz.java
@@ -0,0 +1,14 @@
+package com.lxisoft.game.quiz;
+import com.lxisoft.game.quiz.*;
+import java.io.*;
+public class Quiz 
+{
+	User u;
+	Question que;
+	public Quiz()
+	{
+		u=new User();
+		u.login();
+	}
+	
+}
