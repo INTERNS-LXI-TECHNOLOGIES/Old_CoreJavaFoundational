f401d245dc31adaa2c50d1e43dfdb134ba73a713
p-sreejith <sreejith.p@lxisoft.com>
2018-07-19 09:53:28 +0530

    first commit

---
+++ com/lxisoft/campus/college/Student.java
@@ -0,0 +1,19 @@
+package com.lxisoft.campus.college;
+public class Student
+{
+	private String name;
+	void setStudName(String name)
+	{
+		this.name=name;
+	}
+		
+		
+	void display()
+	{
+		System.out.println(this.name);
+	}
+}	
+	
+	
+	
+	
---
+++ com/lxisoft/campus/test/Tdd.java
@@ -0,0 +1,36 @@
+package com.lxisoft.campus.test;
+import  com.lxisoft.campus.college.*;
+import java.util.*;
+public class Tdd
+{
+	Scanner scan=new Scaner(System.in);
+	public static void main(String args[])
+	{	
+	int choice;
+	System.out.println("enter your choice");
+	choice=scan.nextInt();
+	
+	{
+		
+	
+	 scan    
+		
+		College SREEPATHY=new College();
+		SREEPATHY.setClgName("Sreepathy");
+		switch (choice)
+		{
+			
+		case 1:SREEPATHY.setDepartment();
+		case 2:SREEPATHY.setClass();
+		case 4:SREEPATHY.setStudent();
+		SREEPATHY.setStudent();
+		SREEPATHY.display();
+				
+	}
+	
+}	
+			
+			
+			
+			
+		
