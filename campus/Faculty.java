5f1c10bcbc27b7a6b23a95bbf22d084ac507b221
aparna-udayakumar <aparna.u@lxisoft.com>
2018-07-19 14:02:57 +0530

    program using scanner class completed

---
+++ campus/Faculty.java
@@ -0,0 +1,21 @@
+package com.lxisoft.campus;
+import java.util.*;
+public class Faculty
+{
+	public static void main(String args[])
+	{
+		String name;
+		String dept;
+		Scanner ob=new Scanner(System.in);
+		System.out.println("Enter the Faculty name:");
+		name=ob.nextLine();
+		System.out.println("Enter the department name:");
+		dept=ob.nextLine();
+		System.out.println("faculty name:"+name);
+		System.out.println("faculty department:"+dept);
+		
+		
+		
+		
+	}
+}
