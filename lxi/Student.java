3860edcebea3e64c6cd40a570ab26f8e0b57b45f
aparna-udayakumar <aparna.u@lxisoft.com>
2018-07-19 12:25:57 +0530

    student class is modified

--- lxi/Student.java
+++ lxi/Student.java
@@ -1,12 +1,12 @@
 class Student
 {
 	String name;
-	int id;
+	
 
 	  void say1()
 	     {
 	        System.out.println(" Student name:" +name);
-	         System.out.println(" Student id:" +name);
+	        
 	        
 	     }
 }
