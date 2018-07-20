579df00518de02abf88334a6efb4ae600eff25e9
ArthadhKallingal <arthadh.b@lxisoft.com>
2018-07-20 23:25:59 +0530

    LI-66

---
+++ campusv2/Campus.java
@@ -0,0 +1,230 @@
+package com.lxisoft.campusv2;
+import com.lxisoft.campusv2.faculty.Faculty;
+import com.lxisoft.campusv2.student.StudentEntry;
+import java.util.Scanner;
+public class Campus
+{
+public static void main (String args[])
+{
+	Faculty f=new Faculty();
+	StudentEntry se=new StudentEntry();
+Scanner sc=new Scanner(System.in);
+System.out.println("-----\t\tChoose the Department----\n1)CSE\n2)MECH");
+int choice=sc.nextInt();
+switch(choice)
+{
+case 1:		System.out.println("You chose CSE");
+			Scanner cs=new Scanner(System.in);
+           System.out.println("\t\t----choose the batch----\n1)First year\n2)Second Year\n3)Third year\n4)Fourth year");
+            int ch=sc.nextInt();
+             switch(ch)
+            {
+             case 1:System.out.println("You chose first year\n");
+					System.out.println("Are you Student or Faculty ? \n\t1)Faculty\n\t2)Student");
+					 int ch1=sc.nextInt();
+					 switch(ch1)
+                     {
+						 
+                      case 1:
+					  System.out.println("\nEnter the details of faculty");
+					        System.out.println("\nEnter the name:\t ");
+							String name=cs.nextLine();
+							f.setname(name);
+							System.out.println("\nEnter the address:\t ");
+						    String address=cs.nextLine();
+							f.setaddress(address);
+							System.out.println("\nEntered name is:\t"+f.getname());
+							System.out.println("\nEntered address is:\t"+f.getaddress());
+                      break;
+					  case 2:System.out.println("Enter student details");
+					  se.entry();
+                      break;
+					  default:System.out.println("invalid choice");
+					  break;
+					 }
+					 break;
+             case 2:System.out.println("You chose second year\n");
+                    System.out.println("Are you Student or Faculty ? \n\t1)Faculty\n\t2)Student");
+					 int ch2=sc.nextInt();
+					 switch(ch2)
+                     {
+                      case 1:System.out.println("\nEnter the details of faculty");
+					        System.out.println("\nEnter the name:\t ");
+							String name=cs.nextLine();
+							f.setname(name);
+						
+							 System.out.println("\nEnter the address:\t ");
+						   String address=cs.nextLine();
+							f.setaddress(address);
+							System.out.println("\nEntered name is:\t"+f.getname());
+							System.out.println("\nEntered address is:\t"+f.getaddress());
+                      break;
+					  case 2:System.out.println("Enter student details");
+					  se.entry();
+                      break;
+					  default:System.out.println("invalid choice");
+					  break;
+					 }
+					 break;
+			 case 3:System.out.println("you chose Third year");
+                    System.out.println("Are you Student or Faculty ? \n\t1)Faculty\n\t2)Student");
+					 int ch3=sc.nextInt();
+					 switch(ch3)
+                     {
+                      case 1:System.out.println("\nEnter the details of faculty");
+					        System.out.println("\nEnter the name:\t ");
+							String name=cs.nextLine();
+							f.setname(name);
+						
+							 System.out.println("\nEnter the address:\t ");
+						   String address=cs.nextLine();
+							f.setaddress(address);
+							System.out.println("\nEntered name is:\t"+f.getname());
+							System.out.println("\nEntered address is:\t"+f.getaddress());
+                      break;
+					  case 2:System.out.println("Enter student details");
+					  se.entry();
+                      break;
+					  default:System.out.println("invalid choice");
+					  break;
+					 }
+					 break;
+			 case 4:System.out.println("\nYou chose fourth year");
+		             System.out.println("Are you Student or Faculty ?\n\t1)faculty\n\t2)student)");
+					 int ch4=sc.nextInt();
+					 switch(ch4)
+                     {
+                      case 1:System.out.println("\nEnter the details of faculty");
+					        System.out.println("\nEnter the name:\t ");
+							String name=cs.nextLine();
+							f.setname(name);
+						
+							 System.out.println("\nEnter the address:\t ");
+						   String address=cs.nextLine();
+							f.setaddress(address);
+							System.out.println("\nEntered name is:\t"+f.getname());
+							System.out.println("\nEntered address is:\t"+f.getaddress());
+                      break;
+					  case 2:System.out.println("Enter student details");
+					  se.entry();
+                      break;
+					  default:System.out.println("\ninvalid choice");
+					  break;
+					 }
+					 break;
+             default:System.out.println("invalid choice");
+				break;
+            }
+			break;
+        
+case 2:System.out.println(" You chose mech");
+Scanner me=new Scanner(System.in);
+           System.out.println("\t\t----choose the batch----\n1)First year\n2)Second Year\n3)Third year\n4)Fourth year");
+            int c=sc.nextInt();
+             switch(c)
+            {
+             case 1:System.out.println("You chose first year\n");
+					System.out.println("Are you Student or Faculty ? \n\t1)Faculty\n\t2)Student");
+					 int ch11=sc.nextInt();
+					 switch(ch11)
+                     {
+						 
+                      case 1:
+					  System.out.println("\nEnter the details of faculty");
+					        System.out.println("\nEnter the name:\t ");
+							String name=me.nextLine();
+							f.setname(name);
+							
+							 System.out.println("\nEnter the address:\t ");
+						   String address=me.nextLine();
+							f.setaddress(address);
+							System.out.println("\nEntered name is:\t"+f.getname());
+							System.out.println("\nEntered address is:\t"+f.getaddress());
+                      break;
+					  case 2:System.out.println("Enter student details");
+					  se.entry();
+                      break;
+					  default:System.out.println("invalid choice");
+					  break;
+					 }
+					 break;
+             case 2:System.out.println("You chose second year\n");
+                    System.out.println("Are you Student or Faculty ? \n\t1)Faculty\n\t2)Student");
+					 int ch12=sc.nextInt();
+					 switch(ch12)
+                     {
+                      case 1:System.out.println("\nEnter the details of faculty");
+					        System.out.println("\nEnter the name:\t ");
+							String name=me.nextLine();
+							f.setname(name);
+						
+							 System.out.println("\nEnter the address:\t ");
+						   String address=me.nextLine();
+							f.setaddress(address);
+							System.out.println("\nEntered name is:\t"+f.getname());
+							System.out.println("\nEntered address is:\t"+f.getaddress());
+                      break;
+					  case 2:System.out.println("Enter student details");
+					  se.entry();
+                      break;
+					  default:System.out.println("invalid choice");
+					  break;
+					 }
+					 break;
+			 case 3:System.out.println("you chose Third year");
+                    System.out.println("Are you Student or Faculty ? \n\t1)Faculty\n\t2)Student");
+					 int ch13=sc.nextInt();
+					 switch(ch13)
+                     {
+                      case 1:System.out.println("\nEnter the details of faculty");
+					        System.out.println("\nEnter the name:\t ");
+							String name=me.nextLine();
+							f.setname(name);
+						
+							 System.out.println("\nEnter the address:\t ");
+						   String address=me.nextLine();
+							f.setaddress(address);
+							System.out.println("\nEntered name is:\t"+f.getname());
+							System.out.println("\nEntered address is:\t"+f.getaddress());
+                      break;
+					  case 2:System.out.println("Enter student details");
+					  se.entry();
+                      break;
+					  default:System.out.println("invalid choice");
+					  break;
+					 }
+					 break;
+			 case 4:System.out.println("\nYou chose fourth year");
+		             System.out.println("Are you Student or Faculty ?\n\t1)faculty\n\t2)student)");
+					 int ch14=sc.nextInt();
+					 switch(ch14)
+                     {
+                      case 1:System.out.println("\nEnter the details of faculty");
+					        System.out.println("\nEnter the name:\t ");
+							String name=me.nextLine();
+							f.setname(name);
+						
+							 System.out.println("\nEnter the address:\t ");
+						   String address=me.nextLine();
+							f.setaddress(address);
+							System.out.println("\nEntered name is:\t"+f.getname());
+							System.out.println("\nEntered address is:\t"+f.getaddress());
+                      break;
+					  case 2:System.out.println("Enter student details");
+					  se.entry();
+                      break;
+					  default:System.out.println("\ninvalid choice");
+					  break;
+					 }
+					 break;
+             default:System.out.println("invalid choice");
+				break;
+            }
+       break;
+default:System.out.println("invalid");
+
+}
+
+
+}
+}
