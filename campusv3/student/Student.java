c12fcba79419accfec6ff6aeb51006493662612b
ArthadhKallingal <arthadh.b@lxisoft.com>
2018-07-23 23:42:50 +0530

    LI_139

---
+++ campusv3/student/Student.java
@@ -0,0 +1,27 @@
+package com.lxisoft.campusv3.student;
+
+public class Student
+{
+	private String name;
+	private String address;
+	
+	public void setname(String name1)
+	{
+		this.name=name1;
+	}
+	
+	public void setaddress(String address1)
+	{
+		this.address=address1;
+	}
+	
+	public String getname()
+	{
+		return name;
+	}
+
+	public String getaddress()
+	{
+		return address;
+	}
+}
