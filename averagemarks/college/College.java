d99075ac60abdc1bccc9e8ba769ea952d21f958d
athulya <athulya.somakumaran@lxisoft.com>
2018-07-23 21:06:59 +0530

    averagemarks

---
+++ averagemarks/college/College.java
@@ -0,0 +1,13 @@
+package com.lxisoft.averagemarks.college;
+import com.lxisoft.averagemarks.college.Student;
+public class College
+{
+public void ave()
+
+{
+	Student st=new Student();
+	st.inputDetails();
+	
+}
+
+}
