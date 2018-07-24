63d7b7cc02bd8d26a5720a8ad36a7f7c3fc2d18e
Varsha <varsha@lxisoft.com>
2018-07-24 23:17:20 +0530

    LI-124

---
+++ campus2/fac/Faculty.java
@@ -0,0 +1,31 @@
+package com.lxisoft.campus2.fac;
+
+public class Faculty
+{
+
+private String name;
+private String facid;
+
+	
+public void setname(String name1)
+{
+this.name=name1;
+}
+public void setfacid(String facid1)
+{
+this.facid=facid1;
+}
+public String getname()
+{
+
+return name;
+}
+public String getfacid()
+{
+	
+
+return facid;
+
+}
+
+} 
