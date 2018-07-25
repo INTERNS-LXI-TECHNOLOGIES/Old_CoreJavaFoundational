fd0f7d1012fac0aa9d40489d26893f7b8b2b0c4a
aparna-udayakumar <aparna.u@lxisoft.com>
2018-07-25 10:32:40 +0530

    faculty salary completed LI-154

---
+++ markentry/college/faculty/FacultySal.java
@@ -0,0 +1,20 @@
+package com.lxisoft.markentry.college.faculty;
+import java.util.*;
+
+public class FacultySal
+
+{
+	public void salary()
+	{
+			
+		    Scanner sn=new Scanner(System.in);
+			System.out.println("enter the no: of session that you have taken:");
+			int h=sn.nextInt();
+			int hr=h*500;
+			System.out.println("your salary is:" +hr);
+			
+		
+		
+	}
+	
+}
