83d92d1be1b1ec214aff2a22fe44630e56b997fd
Varsha <varsha@lxisoft.com>
2018-07-26 21:52:00 +0530

    LI-191

---
+++ metrotrain/train/passenger/Passenger.java
@@ -0,0 +1,77 @@
+package com.lxisoft.metro.station.train.passenger;
+
+public class Passenger
+{
+	private String name;
+	private int age;
+	private char gender;
+	private String bstation;
+	private String dstation;
+	private int fare;
+	
+
+
+public void setName(String n1)
+	{
+		this.name=n1;
+	
+	}
+	
+	public void setAge(int a1)
+	{
+		this.age=a1;
+	}
+	
+	public void setGender(char g)
+	{
+		this.gender=g;
+	}
+	
+	public void setBname(String bs)
+	{
+		this.bstation=bs;
+	}
+	
+	public void setDname(String ds)
+	{
+		this.dstation=ds;
+	}
+	
+	public void setFare(int f1)
+	{
+		this.fare=f1;
+	}
+	
+	
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public int getAge()
+	{
+		return age;
+	}
+	
+	public char getGender()
+	{
+		return gender;
+	}
+	
+	public String getBname()
+	{
+		return bstation;
+	}
+	
+	public String getDname()
+	{
+		return dstation;
+	}
+	
+	public int getFare()
+	{
+		return fare;
+	}
+	
+}
