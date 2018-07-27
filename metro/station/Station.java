8fda9d03f873f5a8759c25580c264ca594f438e1
ArthadhKallingal <arthadh.b@lxisoft.com>
2018-07-27 21:12:15 +0530

    LI-216

---
+++ metro/station/Station.java
@@ -0,0 +1,9 @@
+package com.lxisoft.metro.station;
+public class Station
+{
+	public void kochi()
+	{
+		System.out.println("Welcome to Kochi Metro");
+	}
+	
+}
---
+++ metro/test/Tdd.java
@@ -0,0 +1,16 @@
+package com.lxisoft.metro.test;
+import com.lxisoft.metro.station.Station;
+import com.lxisoft.metro.train.Train;
+public class Tdd
+	{
+	public static void main(String[] args)
+	{
+	
+		Station stname=new Station();
+		stname.kochi();
+		Train tname=new Train();
+		tname.trainDetails();
+		
+
+	}
+}
