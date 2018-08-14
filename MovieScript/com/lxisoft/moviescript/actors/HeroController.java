e7598dc99e35d587c2caf2e80fd182d48ec2f33c
Deepthievijayan <deepthi.e@lxisoft.com>
2018-08-14 14:17:31 +0530

    LI-648

--- MovieScript/com/lxisoft/moviescript/actors/HeroController.java
+++ MovieScript/com/lxisoft/moviescript/actors/HeroController.java
@@ -1,6 +1,9 @@
 package com.lxisoft.moviescript.actors;
 import java.util.Scanner;
-public class HeroController
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
+public class HeroController 
 {
 	private Hero hero;
 	private HeroView heroview;
@@ -11,14 +14,28 @@ public class HeroController
 	}
 	public void heroDetail()
 	{
+		try
+		{
+		File herofile=new File("./com/lxisoft/moviescript/file/hero.txt");
+		FileWriter fwhero=new FileWriter(herofile); 
 		Scanner input=new Scanner(System.in);
 		heroview=new HeroView();
 		heroview.askHeroName();
 		heroName=input.nextLine();
+		fwhero.write(heroName);
+		String newLine=("\n");
+		fwhero.write(newLine);
 		heroview.askHeroCharaName();
 		heroCharaName=input.nextLine();
+		fwhero.write(heroCharaName);
+		fwhero.close();
 		hero=new Hero();
 		hero.setHeroName(heroName);
 		hero.setHeroCharaName(heroCharaName);
+		}
+		catch(IOException e)
+		{
+			System.out.println("IO Exception");
+		}
 	}
 }
--- MovieScript/com/lxisoft/moviescript/actors/VillainController.java
+++ MovieScript/com/lxisoft/moviescript/actors/VillainController.java
@@ -1,5 +1,8 @@
 package com.lxisoft.moviescript.actors;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 public class VillainController
 {
 	private Villain villain;
@@ -11,14 +14,28 @@ public class VillainController
 	}
 	public void villainDetail()
 	{
+		try
+		{
+		File villainfile=new File("./com/lxisoft/moviescript/file/villain.txt");
+		FileWriter fwvillain=new FileWriter(villainfile); 
 		Scanner input=new Scanner(System.in);
 		villainview=new VillainView();
 		villainview.askVillainName();
 		villainName=input.nextLine();
+		fwvillain.write(villainName);
+		String newLine=("\n");
+		fwvillain.write(newLine);
 		villainview.askVillainCharaName();
 		villainCharaName=input.nextLine();
+		fwvillain.write(villainCharaName);
+		fwvillain.close();
 		villain=new Villain();
 		villain.setVillainName(villainName);
 		villain.setVillainCharaName(villainCharaName);
+		}
+		catch(IOException e)
+		{
+			System.out.println("IOException");
+		}
 	}
 }
