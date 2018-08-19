eefd74d5bc74cbe85344afaf7f049de6caf7749a
manoj <manoj.g@lxisoft.com>
2018-08-19 18:24:41 +0530

    LI-658

--- animalGame/game/AnimalGame.java
+++ animalGame/game/AnimalGame.java
@@ -6,6 +6,7 @@ import com.lxisoft.animalGame.game.animal.carnivorous.Lion;
 import com.lxisoft.animalGame.game.animal.carnivorous.Tiger;
 import com.lxisoft.animalGame.game.animal.herbivorous.Rabbit;
 import com.lxisoft.animalGame.game.animal.herbivorous.Deer;
+import com.lxisoft.animalGame.game.animal.herbivorous.Elephant;
 public class AnimalGame
 
 {  
@@ -15,6 +16,7 @@ public class AnimalGame
 	Tiger tiger=new Tiger();
 	Rabbit rabbit=new Rabbit();
 	Deer deer=new Deer();
+	Elephant elephant=new Elephant();
 	ForestControl forest=new ForestControl();
 	Scanner scan=new Scanner(System.in);
 	public void gaming()
@@ -24,9 +26,12 @@ public class AnimalGame
 		lion.create();
 		rabbit.create();
 		deer.create();
+		elephant.create();
 		forest.setAnimalLocation(lion);
 		forest.getForeststatus();
 		forest.setAnimalLocation(tiger);
+		forest.getForeststatus();
+		forest.setAnimalLocation(elephant);
 		forest.getForeststatus();
 		forest.setAnimalLocation(rabbit);
 		forest.getForeststatus();
@@ -34,23 +39,25 @@ public class AnimalGame
 		forest.getForeststatus();
 		distance();
 		animalLocation();
-		forest.animalFight(deer,tiger);
+		forest.animalFight(elephant,tiger);
 		forest.getForeststatus();
 		forest.animalFight(rabbit,deer);
 		forest.getForeststatus();
 	}
 	 void animalLocation()
 	 {
-		System.out.println("Enter the name of the animal to get the location(1-Lion,2-Tiger,3-Rabbit,4-Deer)");
+		System.out.println("Enter the name of the animal to get the location(1-Tiger,2-Lion,3-Rabbit,4-Deer,5-Elephant)");
 		animalCount=scan.nextInt();
-		if(animalCount==1)
-		{forest.getanimallocation(lion);}
 		if(animalCount==2)
+		{forest.getanimallocation(lion);}
+		if(animalCount==1)
 		{forest.getanimallocation(tiger);}
 		if(animalCount==3)
 		{forest.getanimallocation(rabbit);}
 		if(animalCount==4)
 		{forest.getanimallocation(deer);}
+		if(animalCount==5)
+		{forest.getanimallocation(elephant);}
 	 }
 	 void distance()
 	 {
---
+++ animalGame/game/animal/herbivorous/Elephant.java
@@ -0,0 +1,26 @@
+package com.lxisoft.animalGame.game.animal.herbivorous;
+import com.lxisoft.animalGame.game.animal.Animal;
+public class Elephant extends Animal implements Herbivorous
+{
+	
+	
+	public String type;
+	
+	public void eaten()
+	{
+	}
+	
+	public void create()
+		{
+		int Energylevel,Hungrylevel;
+		Energylevel=(int)(Math.random()*50)+80;
+		setEnergyLevel(Energylevel);
+		Hungrylevel=(int)(Math.random()*50)+50;
+		setHungryLevel(Hungrylevel);
+		 System.out.println(getEnergyLevel());
+		 setanimaltype("Herbivorous");
+	
+		}
+	
+
+}
