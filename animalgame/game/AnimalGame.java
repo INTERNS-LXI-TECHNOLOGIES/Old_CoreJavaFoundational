07f90528e5746d149991e812fee664d24b33665a
aparna-udayakumar <aparna.u@lxisoft.com>
2018-08-22 12:45:44 +0530

    LI-684

--- animalgame/game/AnimalGame.java
+++ animalgame/game/AnimalGame.java
@@ -2,6 +2,10 @@ package com.lxisoft.animalgame.game;
 import com.lxisoft.animalgame.game.forest.Forest;
 import com.lxisoft.animalgame.game.forest.animal.Animal;
 import  com.lxisoft.animalgame.game.forest.animal.carnivorous.Lion;
+import  com.lxisoft.animalgame.game.forest.animal.carnivorous.Tiger;
+import com.lxisoft.animalgame.game.forest.animal.herbivorous.Deer;
+import com.lxisoft.animalgame.game.forest.animal.herbivorous.Zebra;
+
 import com.lxisoft.animalgame.game.forest.ForestSettings;
 import java.util.*;
 
@@ -9,6 +13,9 @@ public class AnimalGame
 {
 	Animal animal;
 	Lion lion=new Lion();
+	Tiger tiger=new Tiger();
+	Deer deer=new Deer();
+	Zebra zebra=new Zebra();
 	ForestSettings forestdetail=new ForestSettings();
 	Scanner scan=new Scanner(System.in);
 	
@@ -16,6 +23,10 @@ public class AnimalGame
 	{
 		forestdetail.setForest();
 		forestdetail.setAnimalLocation(lion);
+		forestdetail.setAnimalLocation(tiger);
+		forestdetail.setAnimalLocation(deer);
+		forestdetail.setAnimalLocation(zebra);
+		
 		forestdetail.setForestStatus();
 	}
 		
---
+++ animalgame/game/forest/animal/herbivorous/Deer.java
@@ -0,0 +1,10 @@
+package com.lxisoft.animalgame.game.forest.animal.herbivorous;
+import com.lxisoft.animalgame.game.forest.animal.Animal;
+
+public class Deer extends Animal
+{
+	private String animalName="deer";
+	
+	
+}
+
