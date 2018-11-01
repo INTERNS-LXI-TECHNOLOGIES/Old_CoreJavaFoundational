89f09b42b155116d621abedc641763a6ca7042c1
Sarathkrishna S <sarathkrishna.s@lxisoft.com>
2018-11-01 15:48:19 +0530

    Test Commit

---
+++ State.java
@@ -0,0 +1,15 @@
+class State
+{
+Char population;
+String name;
+}
+class stateS
+{
+public static void main(String [] args)
+{
+State s = new State();
+s.population="C";
+s.name="Karnataka";
+System.out.println(s.name+""+s.population);
+}
+}
