094076f1216513494cdd95ffa210ea70ec67c93f
aparna-udayakumar <aparna.u@lxisoft.com>
2018-11-10 10:54:57 +0530

    modified jdbc project:

--- jdbcproject/CustomerController.java
+++ jdbcproject/CustomerController.java
@@ -4,6 +4,8 @@ import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.*;
+import java.sql.*;
 class CustomerController
 {
 	public static void main(String[] args)
@@ -14,9 +16,24 @@ class CustomerController
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/customerlogin","root","root");
 			
-		
+			
 			Statement stmt=con.createStatement();
-			String query="select * from customerdetails";
+	        String query="select * from customerdetails";
+			
+			PreparedStatement pstmt=con.prepareStatement("insert into customerdetails values(?,?)");
+			
+			Scanner scanner= new Scanner(System.in);
+			System.out.println("enter the id:");
+			int id=scanner.nextInt();
+			
+			System.out.println("enter the username:");
+			String username=scanner.next();
+			
+			pstmt.setInt(1,id);
+			pstmt.setString(2,username);
+			
+			int j=pstmt.executeUpdate();
+			System.out.println(j+"record inserted");
 			
 			ResultSet set =stmt.executeQuery(query);
 			while(set.next())
