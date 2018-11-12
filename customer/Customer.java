7d22ddfa5c15403d8b310761e5b41582fec7ee91
athulya <athulya.somakumaran@lxisoft.com>
2018-11-12 22:18:36 +0530

    LI-981

--- customer/Customer.java
+++ customer/Customer.java
@@ -1,5 +1,7 @@
 package com.lxisoft.customer;
 import java.sql.*;
+import java.util.Scanner;
+
 class Customer
 {
 	public static void main(String[] args)
@@ -7,14 +9,31 @@ class Customer
 		try
 		{
 		Class.forName("com.mysql.jdbc.Driver");
+		Scanner scan=new Scanner(System.in);
 		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/user","root","root");
-		Statement stmt=con.createStatement();
-		ResultSet result=stmt.executeQuery("Select * from customer");
-		//System.out.println(result.getInt("customer_id"));
-		while(result.next())
+		//Statement stmt=con.createStatement("Select * from customer");
+		
+		PreparedStatement prepStmt=con.prepareStatement("Insert into customer values(?,?)");
+		System.out.println("Enter customer Id");
+		int id=scan.nextInt();
+		System.out.println("Enter customer name");
+		String name=scan.next();
+		
+		prepStmt.setInt(1,id);
+		prepStmt.setString(2,name);
+		
+		int status=prepStmt.executeUpdate();
+		System.out.print(status);
+		
+		PreparedStatement prepared=con.prepareStatement("select * from customer");
+		ResultSet rs=prepared.executeQuery();
+		
+		
+		while(rs.next())
 		{
-		System.out.println(result.getInt("customer_id")+result.getString("customer_name"));
+		System.out.println(rs.getInt("customer_id")+rs.getString("customer_name"));
 		}
+		
 		}
 		catch(Exception e)
 		{
