778ac8819668a0b45ff974ed0451cfd56eab6d46
AryaVineesh <arya.p@lxisoft.com>
2018-12-24 12:44:34 +0530

    LI-1018

--- quiz_v1/com/lxisoft/quiz/questionpaper/Questionpaper.java
+++ quiz_v1/com/lxisoft/quiz/questionpaper/Questionpaper.java
@@ -5,7 +5,8 @@ import com.lxisoft.quiz.questionpaper.Questions;
 public class Questionpaper
 {
 	ArrayList<Questions> questions=new ArrayList<Questions>();
-	int count;
+	int count,Mark=0;
+	
 	String temp;
 	public void createQuestionPaper()
 	{
@@ -15,47 +16,46 @@ public class Questionpaper
 		FileReader fr=new FileReader(quizFile);
 		BufferedReader br=new BufferedReader(fr);
 		String s=br.readLine();
-		
-			File answerFile=new File("answerFile.csv");
-			answerFile.createNewFile();
-			
-			
-			
-			FileWriter fw=new FileWriter(answerFile);
-			BufferedWriter bw=new BufferedWriter(fw);
 			while(s!=null)
 			{
 				
 				String stringarray[]=s.split(",");
-				for(String temp:stringarray)
-				System.out.println(temp);
+				for(int i=0;i<5;i++)
+				
+				System.out.println(stringarray[i]);
 				System.out.println("Enter your answer:\n");
 				Scanner sc=new Scanner(System.in);
 				String readAnswer=sc.nextLine();
-
-					bw.write(readAnswer);
+				if(readAnswer.equals(stringarray[5]))
+				{
+					count++;
+					Mark=Mark+2;
+				}
 					
 				s=br.readLine();
 			}
-			
-			File answer=new File("D:\\lxi_projects\\java programs\\quiz_v1\\answer.csv");
-			FileReader fr1=new FileReader(answer);
-			BufferedReader br1=new BufferedReader(fr1);
-			String s1=br1.readLine();
-			
-			while(s1!=null)
-			{
-				
-				String stringarray1[]=s1.split(",");
-				for(String temp1:stringarray1)
-					if(temp1==temp)
-						count=count+1;
-					else
-						s1=br1.readLine();
-			}
+			System.out.println();
+			System.out.println("You are sucessfully completed.........");
+			if(Mark>0)
+				System.out.println("Status : Pass");
+			else
+				System.out.println("Status : Failed");
 			System.out.println("The no of questions answered is:\n"+count);
-		bw.flush();
-		bw.close();
+			System.out.println("Mark Scored :"+Mark);
+			if(Mark>=9)
+				System.out.println("Grade A");
+			else if(Mark<9 && Mark>=7)
+				System.out.println("Grade B");
+			else if(Mark<7 && Mark>=5)
+				System.out.println("Grade C");
+			else if(Mark<5)
+			{
+				System.out.println("Failed");
+			}
+				
+				
+	
+		
 		
 		
 	}
