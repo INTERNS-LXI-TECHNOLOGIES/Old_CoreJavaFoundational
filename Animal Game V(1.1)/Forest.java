7b8245d8414dc105d74adc8a045d9644de8df882
rahul <rahul.p.r@lxisoft.com>
2019-02-01 16:31:35 +0530

    Animal Game V(1.1)

---
+++ Animal Game V(1.1)/Forest.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Forest{
+
+
+
+ArrayList<Animal> animals1;
+
+
+public void setAnimals1(ArrayList<Animal> animals1) {
+    this.animals1 = animals1;
+}
+public ArrayList<Animal> getAnimals1() {
+    return animals1;
+}
+
+
+ 
+
+    
+}
