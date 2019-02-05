b20fd84f5b965b3b85b68d4b298b99a4cc0030a4
greeshma01 <greeshma.c.g@lxisoft.com>
2019-02-05 17:02:25 +0530

    animal game v2

--- animalGame/com/lxisoft/animalGame/forest/Animal.java
+++ animalGame/com/lxisoft/animalGame/forest/Animal.java
@@ -3,9 +3,25 @@ package com.lxisoft.animalGame.forest;
 public class Animal
 {
     private String name;
-	private int strength;
+	private int strength,locationX,locationY;
 	
 	
+	public void setLocationX(int locationX)
+	{
+		this.locationX=locationX;
+	}
+	public int getLocationX()
+	{
+		return locationX;
+	}
+	public void setLocationY(int locationY)
+	{
+		this.locationY=locationY;
+	}
+	public int getLocationY()
+	{
+		return locationY;
+	}
     public	void setName(String name)
 	{
 		this.name=name;
@@ -21,10 +37,20 @@ public class Animal
 	{
 		return strength; 
 	}
-	public int aa()
+	public int returnStrength()
    { 
 	setStrength((int)(Math.random()*100));
 	return  getStrength();
    }
- 
+  public int location1()
+   {
+	   setLocationX((int)(Math.random()*60));
+	   return getLocationX();
+	   
+   }
+  public int location2()
+   {
+       setLocationY((int)(Math.random()*100));
+	   return getLocationY();
+   }
 }
--- animalGame/com/lxisoft/animalGame/forest/Forest.java
+++ animalGame/com/lxisoft/animalGame/forest/Forest.java
@@ -1,66 +1,110 @@
 package com.lxisoft.animalGame.forest;
 import com.lxisoft.animalGame.forest.*;
-
 public class Forest
 {
     Animal[]animal=new Animal[12];
-		 public void detailsOfAnimal()
-			{
-               int x=1;
-			   for(int i=0;i<3;i++)
-					{
-						animal[i]=new Tiger();
-						animal[i].setName("Tiger"+x+"");
-						x++;
-					}
-					for(int j=3;j<6;j++)
-					{
-						animal[j]=new Lion();
-						animal[j].setName("Lion"+x+"");
-						x++;
-					}
-					for(int k=6;k<9;k++)
-					{
-						animal[k]=new Rabbit();
-						animal[k].setName("Rabbit"+x+"");
-						x++;
-					}
-					for(int l=9;l<12;l++)
-					{
-						animal[l]=new Deer();
-						animal[l].setName("Deer"+x+"");
-						x++;
-					}
+	 public void detailsOfAnimal()
+		{
+             int x=1,y=1,z=1,o=1;
+			 for(int i=0;i<3;i++,x++)
+				{
+				  animal[i]=new Tiger();
+				  animal[i].setName("Tiger"+x+"");
+				}
+			 for(int j=3;j<6;j++,y++)
+				{
+				  animal[j]=new Lion();
+				  animal[j].setName("Lion"+y+"");
+				}
+			for(int k=6;k<9;k++,z++)
+				{
+				  animal[k]=new Rabbit();
+				  animal[k].setName("Rabbit"+z+"");
+				}
 					
-					System.out.println("ANIMALS IN FOREST");
-					System.out.println("==================");
+			for(int l=9;l<12;l++,o++)
+				{
+				  animal[l]=new Deer();
+				  animal[l].setName("Deer"+o+"");
+				}
+			
+			System.out.println("ANIMALS IN FOREST");
+			System.out.println("==================");
 					for(int m=0;m<12;m++)
 					{
 						System.out.println(animal[m].getName());
-						
-					}
-					int a,b;
+				    }
+					int animal1,animal2;
 					System.out.println("************");
 					
-					 a=(int)(Math.random()*12);
-				     b=(int)(Math.random()*12);
-					 System.out.println("a="+animal[a].getName()+" "+animal[a].aa());
-				     System.out.println("b="+animal[b].getName()+" "+animal[b].aa());
+				    animal1=(int)(Math.random()*12);
+				    animal2=(int)(Math.random()*12);
+				
+				
+				 
+				 System.out.println("animal 1=   "+animal[animal1].getName()+"   STRENGTH="+animal[animal1].returnStrength()+"     x1="+animal[animal1].location1()+"    y1="+animal[animal1].location2());
+				 System.out.println("animal 2=   "+animal[animal2].getName()+"   STRENGTH="+animal[animal2].returnStrength()+"     x2="+animal[animal2].location1()+"    y2="+animal[animal2].location2());
+                 int  x1=animal[animal1].getLocationX();
+			     int  x2=animal[animal2].getLocationX();
+	             int  y1=animal[animal1].getLocationY();
+			     int  y2=animal[animal2].getLocationY();
+				 
+				
+				if(animal[animal1]!=animal[animal2])
+		  { 
+				 int differance1=x1-x2;
+				 int differance2=y1-y2;
+				 System.out.println("   d1= "+differance1+"   d2=  "+differance2);
+				 if(differance1<0&&differance2<0)
+				 {
+					  differance1=(-(differance1));
+					  System.out.println("d1="+differance1);
+					  differance2=(-(differance2));
+					  System.out.println("d2="+differance2);
+				 }
+				 else if(differance1<0&&differance2>0)
+				 {
+					 differance1=(-(differance1));
+					 System.out.println("d1="+differance1);
+					 
+				 }
+				 else if(differance1>0&&differance2<0)
+				 {
+					 differance2=(-(differance2));
+					 System.out.println("d2="+differance2);
+				 }
 					
-				
-				
-			   
-				
-				if(animal[a].getStrength()>animal[b].getStrength())
-				{
-					System.out.println(animal[a].getName()+animal[a].getStrength());
-					System.out.println("winner is"+animal[a].getName());
-				}
-				else
-				{
-					System.out.println(animal[b].getName()+animal[b].getStrength());
-					System.out.println("winner is"+animal[b].getName());
-				}
-				
-			}
-}
+		 
+				  if(differance1<=10&&differance2<=10)
+				  {  
+					 if(animal[animal1].getStrength()>animal[animal2].getStrength())
+				     {
+						System.out.println("winner is"+animal[animal1].getName());
+				        
+					 }  
+				    else
+				     { 
+						System.out.println("winner is"+animal[animal2].getName());
+				     }
+			      }
+				 
+				    else 
+				    {
+					  System.out.println("will not fight");
+				    }
+		  }
+				 else
+				 {
+					 System.out.println(".........");
+				 }
+		}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	}
