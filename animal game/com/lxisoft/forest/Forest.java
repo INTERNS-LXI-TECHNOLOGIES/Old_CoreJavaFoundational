0528a928de61084358167cd9902c7e4caa404c04
ayana98 <ayana.k.u@lxisoft.com>
2019-02-07 12:44:49 +0530

    completed

--- animal game/com/lxisoft/forest/Forest.java
+++ animal game/com/lxisoft/forest/Forest.java
@@ -1,128 +1,103 @@
 package com.lxisoft.forest;
 import com.lxisoft.animal.*;
+import java.util.ArrayList;
 public class Forest
 {
-Animal[] animals;
+
+ArrayList<Animal> animals=new ArrayList<Animal>();
 
 public void setAnimalDetails()
 {
-	
-
-animals=new Animal[12];
-
 for(int i=0;i<3;i++)
 {
-animals[i]=new Tiger();
-animals[i].setName("Tiger"+(i+1));
+	animals.add(new Tiger());
+	animals.get(i).setName("Tiger"+(i+1));
 }
 
 for(int i=3;i<6;i++)
 {
-animals[i]=new Lion();
-animals[i].setName("Lion"+(i-2));
+	animals.add(new Lion());
+	animals.get(i).setName("Lion"+(i-2));
 }
-	
+
 for(int i=6;i<9;i++)
 {
-	animals[i]=new Rabbit();
-	animals[i].setName("Rabbit"+(i-5));
+	animals.add(new Rabbit());
+	animals.get(i).setName("Rabbit"+(i-5));
 }
 
 for(int i=9;i<12;i++)
 {
-	animals[i]=new Deer();
-	animals[i].setName("Deer"+(i-8));
+	animals.add(new Deer());
+	animals.get(i).setName("Deer"+(i-8));
 }
-	
 System.out.println("AVAILABLE ANIMALS");
 System.out.println("_________________");
 
-
 for(int i=0;i<3;i++)
 {
-	System.out.println(animals[i].getName());
+	System.out.println(animals.get(i).getName());
 }
 
 for(int i=3;i<6;i++)
 {
-	System.out.println(animals[i].getName());
+	System.out.println(animals.get(i).getName());
 }
 
 for(int i=6;i<9;i++)
 {
-	System.out.println(animals[i].getName());
-}
+System.out.println(animals.get(i).getName());
+}	
 
 for(int i=9;i<12;i++)
 {
-	System.out.println(animals[i].getName());
+System.out.println(animals.get(i).getName());
 }
-System.out.println("   ");
-}
-public void fight()
-{
-	int animal1,animal2;
-animal1=(int)(Math.random()*12);
-animal2=(int)(Math.random()*12);
-System.out.println("AVAILABLE ANIMALS WHO IS READY TO FIGHT AND THEIR STRENGTH LEVELS & CORRESPONDING LOCATIONS");
-System.out.println("___________________________________________________________________________________________");
-int strength1=animals[animal1].strength();
-int strength2=animals[animal2].strength();
-int locationX1=animals[animal1].locationX();
-int locationY1=animals[animal1].locationY();
-int locationX2=animals[animal2].locationX();
-int locationY2=animals[animal2].locationY();
-int d1=Math.abs(locationX1-locationX2);
-int d2=Math.abs(locationY1-locationY2);
-System.out.println("Animal="+animals[animal1].getName()+" strength="+strength1+" location="+locationX1+","+locationY1);
-System.out.println("Animal="+animals[animal2].getName()+" strength="+strength2+" location="+locationX2+","+locationY2);
-System.out.println(d1);
-System.out.println(d2);
 
-if(d1<=10 && d2<=10)
+System.out.println("   ");
+System.out.println("AVAILABLE ANIMALS WHO IS READY TO FIGHT AND THEIR STRENGTH LEVELS");
+System.out.println("_________________________________________________________________");
+
+for(int i=0;i<12;i++)
+{
+	int random1=(int)(Math.random()*animals.size());
+	int random2=(int)(Math.random()*animals.size());
+	int strength1=animals.get(random1).strength();
+int strength2=animals.get(random2).strength();
+	
+		System.out.println("Animal="+animals.get(random1).getName()+" Strength="+strength1);
+	System.out.println("Animal="+animals.get(random2).getName()+" Strength="+strength2);
+		
+	
+	if(animals.get(random1).getName()!=animals.get(random2).getName())
+	{
+	
+		if(strength1>strength2)
+		{
+			System.out.println(animals.get(random1).getName()+" win...");
+			System.out.println("  ");
+			animals.remove(random2);
+		}
+		else 
+		{
+		System.out.println(animals.get(random2).getName()+" win...");
+		System.out.println("  ");
+		animals.remove(random1);
+		
+	}
+	
+}
+}
+System.out.println("  ");
+System.out.println("ALIVE ANIMALS");
+System.out.println("_____________");
+for(Animal a:animals)
 {
 	
-	if(strength1>strength2)
-{
-	System.out.println("Will fight");
-	System.out.println(animals[animal1].getName()+" win....");
-}
-else
-{
-	System.out.println(animals[animal2].getName()+" win....");
+	System.out.println(a.getName());
+	
 }
 
-}
-else
-{
-	System.out.println("Will not fight");
-}
-/*if(d1<0 && d2<0)
-{
-	d1=-(d1);
-	d2=-(d2);
-	System.out.println(d1);
-	System.out.println(d2);
-}
-else if(d1<0 || d2>0)
-{
-	d1=-(d1);
-	System.out.println(d1);
-}
-else if(d2<0 || d1>0)
-{
-	d2=-(d2);
-	System.out.println(d2);
-}*/
-}
-/*
-if(strength1>strength2)
-{
-	System.out.println(animals[animal1].getName()+" win....");
-}
-else
-{
-	System.out.println(animals[animal2].getName()+" win....");
-}*/
 
 }
+}	
