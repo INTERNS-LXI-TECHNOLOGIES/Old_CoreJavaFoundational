bb4575f208174317eaf1c40616135321e6fdc52f
pavananp <pavana.n.p@lxisoft.com>
2019-02-11 14:10:11 +0300

    animal game

---
+++ forestv4/Flume.java
@@ -0,0 +1,25 @@
+package com.lxisoft.forestv4;
+public class Flume
+{
+	private int locationx;
+	private int locationy;
+	public void setLocationx(int locationx)
+	{
+		this.locationx=locationx;
+	}
+	public int getLocationx()
+	{
+		return locationx;
+	}
+	public void setLocationy(int locationy)
+	{
+		this.locationy=locationy;
+	}
+	public int getLocationy()
+	{
+		return locationy;
+	}
+}
+	
+		
+	
--- forestv4/Forest.java
+++ forestv4/Forest.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class Forest
 {
 	private ArrayList <Animal> animals;
+	private ArrayList <Flume> flumes;
 	public void setAnimals(ArrayList <Animal> animals)
 	{
 		this.animals=animals;
@@ -13,6 +14,14 @@ public class Forest
 	{
 		return animals;
 	} 
+	public void setFlumes(ArrayList <Flume> flumes)
+	{
+		this.flumes=flumes;
+	}
+	public ArrayList <Flume> getFlumes()
+	{
+		return flumes;
+	}
 	public void animalFight()
 	{
 		setAnimals(new ArrayList <Animal>());
@@ -44,6 +53,14 @@ public class Forest
 	getAnimals().add(new Hunter());
 	getAnimals().get(30).setName("Hunter");
 	getAnimals().get(30).setSpeed(1);
+	 setFlumes(new ArrayList <Flume>());
+	for(int k=0;k<3;k++)
+	{
+		getFlumes().add(new Flume());
+		getFlumes().get(k).setLocationx((int)(Math.random()*60));
+		getFlumes().get(k).setLocationy((int)(Math.random()*60));
+	}
+		
 		int r=1;
 		int meters=60;
 		do
@@ -60,11 +77,28 @@ public class Forest
 		anim.setLocationy((int)(Math.random()*(meters)));
 		anim.setStrength((int)(Math.random()*100));
 		}
+		for(int f=0;f<3;f++)
+		{
 		for(int l=0;l<animals.size();l++)
 		{
 			int random1 =(int)(Math.random()*animals.size()-1);
 		if(animals.get(random1).getLocationx()>0&&animals.get(l).getLocationx()>0&&animals.get(random1).getLocationy()>0&&animals.get(l).getLocationy()>0)
 		{	
+		
+		if((flumes.get(f).getLocationx()==animals.get(random1).getLocationx())&&(flumes.get(f).getLocationy()==animals.get(random1).getLocationy()))
+		{
+			animals.remove(random1);
+			System.out.println("Fall");
+		}
+		else
+		{
+		if((flumes.get(f).getLocationx()==animals.get(l).getLocationx())&&(flumes.get(f).getLocationy()==animals.get(l).getLocationy()))
+		{
+			animals.remove(l);
+			System.out.println("Fall");
+		}
+		else
+		{
 		if(Math.abs((animals.get(random1).getLocationx())-(animals.get(l).getLocationx()))<10&&Math.abs((animals.get(random1).getLocationy())-(animals.get(l).getLocationy()))<10)
 		{	
 		 if(animals.get(random1) instanceof Herbivore&&animals.get(l) instanceof Carnivore)
@@ -76,8 +110,10 @@ public class Forest
 			{
 				//System.out.println("Luck factor:"+luckFactor);
 			System.out.println(animals.get(random1).getName()+" escaped from the location");
-			//animals.get(random1).setLocationx(animals.get(random1).getLocationx()+animals.get(random1).getSpeed());
-			//System.out.println(animals.get(random1).setLocationx());
+			animals.get(random1).setLocationx(animals.get(random1).getLocationx()+animals.get(random1).getSpeed());
+			animals.get(random1).setLocationy(animals.get(random1).getLocationy()+animals.get(random1).getSpeed());
+			System.out.print(animals.get(random1).getName()+" Changed locationx "+animals.get(random1).getLocationx());
+			System.out.println(" Changed locationy "+animals.get(random1).getLocationy());
 			}
 			else
 			{
@@ -105,6 +141,10 @@ public class Forest
 			{
 				//System.out.println("Luck factor:"+luckFactor);
 			System.out.println(animals.get(l).getName()+" escaped from the location");
+			animals.get(l).setLocationx(animals.get(l).getLocationx()+animals.get(l).getSpeed());
+			animals.get(l).setLocationy(animals.get(l).getLocationy()+animals.get(l).getSpeed());
+			System.out.print(animals.get(l).getName()+" Changed locationx "+animals.get(l).getLocationx());
+			System.out.println(" Changed locationy "+animals.get(l).getLocationy());
 			}
 			else
 			{ 
@@ -195,6 +235,10 @@ public class Forest
 			else
 			{
 				System.out.println(animals.get(random1).getName()+" escaped from that location");
+				animals.get(random1).setLocationx(animals.get(random1).getLocationx()+animals.get(random1).getSpeed());
+				animals.get(random1).setLocationy(animals.get(random1).getLocationy()+animals.get(random1).getSpeed());
+				System.out.print(animals.get(random1).getName()+" Changed locationx "+animals.get(random1).getLocationx());
+				System.out.println(" Changed locationy "+animals.get(random1).getLocationy());
 			}
 				
 		 }
@@ -211,7 +255,14 @@ public class Forest
 			else
 			{
 				System.out.println(animals.get(l).getName()+" escaped from that location");
+				animals.get(l).setLocationx(animals.get(l).getLocationx()+animals.get(l).getSpeed());
+				animals.get(l).setLocationy(animals.get(l).getLocationy()+animals.get(l).getSpeed());
+				System.out.print(animals.get(l).getName()+" Changed locationx "+animals.get(l).getLocationx());
+				System.out.println(" Changed locationy "+animals.get(l).getLocationy());
 			}	
+		}
+		}
+		}
 		}
 		}
 		}
