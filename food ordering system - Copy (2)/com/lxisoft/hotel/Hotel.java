b5335b9cfe5703c361fc9f659a945cf19082e713
ayana98 <ayana.k.u@lxisoft.com>
2019-03-07 17:20:54 +0530

    LI-114

---
+++ food ordering system - Copy (2)/com/lxisoft/hotel/Hotel.java
@@ -0,0 +1,69 @@
+package com.lxisoft.hotel;
+import com.lxisoft.admin.*;
+import com.lxisoft.fooditem.*;
+import com.lxisoft.customer.*;
+import com.lxisoft.bill.*;
+import java.util.*;
+public class Hotel
+{
+Admin admin=new Admin();
+Customer customer=new Customer();
+public ArrayList<FoodItem> foodItems=new ArrayList<FoodItem>();
+
+Scanner scan=new Scanner(System.in);
+
+public void homePage()
+{
+	int ch,y,price;
+	Bill bill=new Bill();
+	do
+	{
+		
+		      System.out.println("  ");
+			  System.out.println("1.ADMIN LOGIN");
+			  System.out.println("2.FOOD MENU");
+			  System.out.println("3.CUSTOMER LOGIN");
+			  System.out.println("  ");
+			  System.out.print("Enter your choice:");
+			  ch=scan.nextInt();
+			   switch(ch)
+			  {
+				  case 1:admin.logAdmin(foodItems);
+				  break;
+				  case 2:foodMenu();
+				  break;
+				  case 3:customer.logUser(foodItems);
+				    price=admin.calculateBill(customer.item,customer.value,foodItems);
+					bill.setTotalPrice(price);
+					bill.viewBill(customer.item,customer.value,foodItems);
+				  break;
+				   default:System.out.println("Invalid choice made...please try again...!!!");
+			
+			  }
+			  System.out.print("Do you want to continue...?Yes=1/No=0:");
+			  y=scan.nextInt();
+			}while(y==1);
+
+
+
+}
+
+	
+	
+	public void foodMenu()
+	{
+	System.out.println(" ");
+	System.out.println("                               FOOD MENU                               ");
+	System.out.println("-----------------------------------------------------------------------");
+	System.out.println("FoodItem                                                           Price");
+	System.out.println("-----------------------------------------------------------------------");
+	
+	for(FoodItem f:foodItems)
+	{
+		
+		System.out.println(f.getName()+"                                          "+f.getPrice());
+	}
+	}
+	
+	
+}
