a489196f0bb8c370c2c7b83c9086d79483197428
greeshma01 <greeshma.c.g@lxisoft.com>
2019-03-09 15:57:41 +0530

    LI-121

--- hotel v1.1/com/lxisoft/hotel/MenuList.java
+++ hotel v1.1/com/lxisoft/hotel/MenuList.java
@@ -1,16 +1,26 @@
 package com.lxisoft.hotel;
 import java.util.ArrayList;
+import java.io.*;
 public class MenuList
 {
-	public  void listOfFood( ArrayList<Food> foods)
+	File f=new File("A.Txt");
+	FileReader fr;
+	BufferedReader br;
+	String s=null;
+	public  void listOfFood( ArrayList<Food> foods) throws Exception
 	{
 		System.out.println("AVILABLE FOOD");
 	    System.out.println("***************");
 		System.out.println("   FOOD    "+"    PRICE    "+"    COUNT");
-	    for( int i=0;i<foods.size();i++)
-		{
-	     
-		   System.out.println("   "+foods.get(i).getName()+"               "+foods.get(i).getPrice()+"             "+foods.get(i).getCount());
-		}
+	   
+			fr=new FileReader(f);
+			br=new BufferedReader(fr);
+	     //	System.out.println("          "+foods.get(i).getName()+"               "+foods.get(i).getPrice()+"             "+foods.get(i).getCount());
+		    while((s=br.readLine())!=null){
+			   System.out.println(">>>>>"+s);
+		   }
+		    
+		
 	}
 }
+//""+foods.get(i).getName()+""+foods.get(i).getPrice()+""+foods.get(i).getCount()
