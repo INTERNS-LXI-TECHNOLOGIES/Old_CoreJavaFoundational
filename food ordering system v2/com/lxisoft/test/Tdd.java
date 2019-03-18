8eb78f4b9f06d154e27c65f5126c9c192684d134
ayana98 <ayana.k.u@lxisoft.com>
2019-03-18 17:20:35 +0530

    LI-189

---
+++ food ordering system v2/com/lxisoft/test/Tdd.java
@@ -0,0 +1,12 @@
+package com.lxisoft.test;
+import com.lxisoft.hotel.*;
+import java.io.*;
+public class Tdd
+{
+public static void main(String args[])throws Exception
+{
+Hotel hotel=new Hotel();
+hotel.homePage();
+
+}
+}
