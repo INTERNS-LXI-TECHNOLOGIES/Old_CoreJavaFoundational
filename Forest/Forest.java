e9b9c3fcc074ecb533147eeffb61786b8bc50fe2
sanjanamuralidhar <sanjanamurilidhar@gmail.com>
2019-03-21 16:49:57 +0530

    Li-190

--- Forest/Forest.java
+++ Forest/Forest.java
@@ -4,16 +4,16 @@ public void fighters()
 {
 	Animal animal=new Animal();
 	
-	Tiger tiger=new Tiger();
+	Animal tiger=new Tiger();
 	tiger.name="Tiger";
 	tiger.animalStrength=70;
-	Rabbit rabbit=new Rabbit();
+	Animal rabbit=new Rabbit();
 	rabbit.name="Rabbit";
 	rabbit.animalStrength=80;
-	Bear bear=new Bear();
+	Animal bear=new Bear();
 	bear.name="Bear";
 	bear.animalStrength=90;
-	Lion lion=new Lion();
+	Animal lion=new Lion();
 	lion.name="Lion";
 	lion.animalStrength=40;
 	animal.fight(tiger,rabbit,bear,lion);
