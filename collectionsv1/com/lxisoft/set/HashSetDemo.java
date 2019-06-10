c3de9ef634190445d0f971bc05825e8672159a54
ayana98 <ayana.k.u@lxisoft.com>
2019-06-10 16:31:29 +0530

    collection

---
+++ collectionsv1/com/lxisoft/set/HashSetDemo.java
@@ -0,0 +1,128 @@
+package com.lxisoft.set;
+import java.util.*;
+public class HashSetDemo
+{
+HashSet<String> programmingLanguages = new HashSet<String>();
+
+	Scanner scan=new Scanner(System.in);
+	
+		public void setOperations()
+		{
+		    int x,choice; 
+			do
+			{
+				System.out.println("----------------------------------------------------------------");
+				System.out.println("                      HASHSET OPERATIONS                        ");
+				System.out.println("----------------------------------------------------------------");
+			    System.out.println("1.ADD");
+				System.out.println("2.DISPLAY");
+				System.out.println("3.DELETE");
+				System.out.println("4.SORT");
+				System.out.println("5.UPDATE");
+				System.out.println("  ");
+			    System.out.print("Enter your choice:");
+			    choice=scan.nextInt();
+				
+				 switch(choice)
+			  {
+				 case 1:add();
+				 break;
+				 case 2:display();
+				 break;
+				 case 3:delete();
+				 break;
+				 case 4:sort();
+				 break;
+				 case 5:update();
+				 break;
+				 default:System.out.println("Invalid choice made...please try again...!!!");
+			
+			  }
+			  System.out.print("want to do any other operation...?Yes=1/No=0:");
+			  x=scan.nextInt();
+			}while(x==1);
+		}
+		
+		public void add()
+		{
+		  int num;
+		  
+	    System.out.println(" ");
+        System.out.print("No.of Items you want to add..?:");
+		num=scan.nextInt();
+		for(int i=0;i<num;i++)
+		{
+		System.out.print("Language "+(i+1)+" : ");
+		programmingLanguages.add(scan.next());
+		}
+		
+	    System.out.println("   ");
+		System.out.println("Record saved.....");
+		System.out.println("   ");
+		}
+		
+		public void display()
+		{
+		Iterator<String> programmingLanguageIterator = programmingLanguages.iterator();
+		System.out.println("   ");
+		System.out.println("   PROGRAMMING LANGUAGES   ");
+		System.out.println("---------------------------");
+        while (programmingLanguageIterator.hasNext())
+		{
+            String programmingLanguage = programmingLanguageIterator.next();
+            System.out.println(programmingLanguage);
+        }
+		}
+		
+		public void delete()
+		{
+			System.out.print("Which item do you want to delete...? :");
+			programmingLanguages.remove(scan.next());
+			System.out.println(" ");
+			System.out.println("Item deleted");
+			System.out.println(" ");
+		}
+			
+			public void sort()
+			{
+				List<String> list =new ArrayList<String>(programmingLanguages);
+                System.out.println(" ");
+				Collections.sort(list);
+				System.out.println("Elements in sorted order : "+list);
+			}
+			
+			public void update()
+			{
+				String ele;
+				int index;
+				
+				List<String> ls=new ArrayList<String>(programmingLanguages);
+				//System.out.println("Elements => "+ls);
+				System.out.print("Which index you want to update...? :");
+				index=scan.nextInt();
+				System.out.print("Type here to update : ");
+				ele=scan.next();
+				ls.set(index,ele);
+				
+				/*for(int i=0;i<ls.size();i++)
+				{
+					
+					if(ele.equals(ls))
+					{
+						System.out.print("Type here to update : ");
+						item=scan.next();
+						ls.set(i,item);
+					}
+				}*/
+				
+				System.out.println(" ");
+				System.out.println("        MODIFIED PROGRAMMING LANGUAGES         ");
+				System.out.println("-----------------------------------------------");
+				for(int i=0;i<ls.size();i++)
+				{
+					System.out.println(ls.get(i));
+				}
+			}
+		
+			
+		}
