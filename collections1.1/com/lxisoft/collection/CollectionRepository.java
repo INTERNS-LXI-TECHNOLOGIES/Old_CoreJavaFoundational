90cb9c13b28813cb2bac83315c62aa73f81c0c1a
ayana98 <ayana.k.u@lxisoft.com>
2019-06-14 16:47:08 +0530

    collection

---
+++ collections1.1/com/lxisoft/collection/CollectionRepository.java
@@ -0,0 +1,9 @@
+package com.lxisoft.collection;
+public interface CollectionRepository
+{
+public void add();
+public void display();
+public void delete();
+public void sort();
+public void update();
+}
