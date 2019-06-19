7eb390d819de4c6801fb418db89f570fb32141b3
ayana98 <ayana.k.u@lxisoft.com>
2019-06-19 16:47:18 +0530

    collection

---
+++ collections1.5/com/lxisoft/collection/CollectionDemo.java
@@ -0,0 +1,61 @@
+package com.lxisoft.collection;
+import java.util.*;
+import com.lxisoft.set.*;
+import com.lxisoft.list.*;
+import com.lxisoft.queue.*;
+import com.lxisoft.map.*;
+
+public class CollectionDemo
+{
+HashSetDemo ha=new HashSetDemo();
+LinkedHashSetDemo lh=new LinkedHashSetDemo();
+TreeSetDemo tr=new TreeSetDemo();
+ArrayListDemo ar=new ArrayListDemo();
+LinkedListDemo lr=new LinkedListDemo();
+PriorityQueueDemo pr=new PriorityQueueDemo();
+HashMapDemo hm=new HashMapDemo();
+Scanner scan=new Scanner(System.in);
+
+public void setOptions()
+{
+ int x,choice;
+			
+			do
+			{
+			System.out.println("  "+"PLEASE CHOOSE A COLLECTION "+"\n"+"  ");
+			System.out.println("1.HASHSET ");
+			System.out.println("2.LINKEDHASHSET ");
+			System.out.println("3.TREESET ");
+			System.out.println("4.ARRAYLIST");
+			System.out.println("5.LINKEDLIST");
+			System.out.println("6.PRIORITYQUEUE");
+			System.out.println("7.HASHMAP ");
+			System.out.println(" ");
+			System.out.print("Enter your choice:");
+			    choice=scan.nextInt();
+				
+				
+				 switch(choice)
+			  {
+				 case 1:ha.setOperations();
+				 break;
+				 case 2:lh.setOperations();
+				 break;
+				 case 3:tr.setOperations();
+				 break;
+				 case 4:ar.setOperations();
+				 break;
+				 case 5:lr.setOperations();
+				 break;
+				 case 6:pr.setOperations();
+				 break;
+				 case 7:hm.setOperations();
+				 break;
+				  default:System.out.println("Invalid choice made...please try again...!!!");
+			
+			  }
+			  System.out.print("want to do any other operation...?Yes=1/No=0:");
+			  x=scan.nextInt();
+			}while(x==1);
+		}
+		}
