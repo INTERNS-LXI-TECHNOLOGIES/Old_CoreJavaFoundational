d3231c25b6dbe5d6369b6753ed958212f4375878
ayana98 <ayana.k.u@lxisoft.com>
2019-07-19 16:45:17 +0530

    snake and ladder

--- snake and ladder/com/lxisoft/game/Game.java
+++ snake and ladder/com/lxisoft/game/Game.java
@@ -11,7 +11,9 @@ Scanner scan=new Scanner(System.in);
 public void playGame()
 {
 	System.out.println("\n");
-	setPlayers();
+	int number=setPlayers();
+	if(number==2 || number==3)
+	{
 	System.out.println("\n"+"There are snakes and ladders on squares...Tread with care...Good Luck....!!!"+"\n");
 	System.out.print("Press any key to start the game... : ");
 	String key=scan.next();
@@ -28,6 +30,24 @@ public void playGame()
 		String str=scan.next();
         int random=dice.getDiceValue();		
 		System.out.println("The number on the die is : "+random);
+		if(players.get(i).getPosition()==0)
+		{
+			if(random==1)
+			{
+				players.get(i).setPosition(random);
+				System.out.println("You have progressed by "+random+" and your new position is "+players.get(i).getPosition());
+		System.out.println("\n"+"==================================================================================="+"\n"+ "                            SNAKE AND LADDER                            " +"\n"+"==================================================================================="+"\n"+"\n");
+
+				board.createBoard(players);
+			}
+			else{
+			System.out.println("you will have to wait for the next turn , you have exeeded 1 ");
+		}
+		}
+		
+		else if(players.get(i).getPosition()>0)
+		{
+				
 		if((players.get(i).getPosition()+random)>100)
 		{
 			System.out.println("you will have to wait for the next turn , you have exeeded 100 ");
@@ -60,11 +80,13 @@ public void playGame()
 		board.createBoard(players);
 		if(players.get(i).getPosition()==100)
 		{
-			System.out.println("congratulations player"+(i+1)+" you have won the game");
+			System.out.println("Congratulations "+players.get(i).getName()+" you have won the game.....");
 			break;
 		}
 	
 		}
+		}
+		
 		}
 		for(int i=0;i<players.size();i++){
 		if(players.get(i).getPosition()==100)
@@ -73,22 +95,40 @@ public void playGame()
 		}
 			}
 	}while(res==0);
+	}
 	
 }
 
-public void setPlayers()
+public int setPlayers()
 {
-	int num;
+	int num=0;
+	do{
+	System.out.println("Maximum no.of player allowed is 3"+"\n");
 	System.out.print("  No.of Players...? :  ");
 	num=scan.nextInt();
 	System.out.println("\n");
+	if(num==2 || num==3)
+	{
 	for(int i=0;i<num;i++)
 		{
 			players.add(new Player());
 			System.out.print("  Name of player"+(i+1)+" : ");
 			players.get(i).setName(scan.next());
-			players.get(i).setPosition(1);
+			players.get(i).setPosition(0);
 		}	
+	}
+	else{
+		if(num==1){
+			System.out.println("Sorry...you can't play this game...Minimum 2 or 3 player is needed..."+"\n"+"Please try again...!!!"+"\n");
+		}
+		else if(num==0 || num<0){
+			System.out.println("Error....!!!"+"\n"+"Please try again...!!!"+"\n");
+		}
+		else{
+			System.out.println("More than 3 players are not allowed to play the game..."+"\n"+"Please try again...!!!"+"\n");
+		}
 }
-
+	}while(num==1 || num>3 || num==0 || num<0);
+return num;
+}
 }
