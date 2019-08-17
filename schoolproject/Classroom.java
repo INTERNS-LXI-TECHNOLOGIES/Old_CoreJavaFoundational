03bd608029481297c686c89099878dd365fa9d76
megha-vt <megha.v.t@lxisoft.com>
2019-08-17 10:34:48 +0530

    task completed

---
+++ schoolproject/Classroom.java
@@ -0,0 +1,14 @@
+public class Classroom
+{
+	int std;
+    Teacher teacher1;
+	Teacher teacher2;
+	
+	 Student student1;
+	 Student student2;
+     Student student3;
+     Student student4;	 
+	
+	
+	
+}
---
+++ schoolproject/Student.java
@@ -0,0 +1,5 @@
+public class Student
+{
+	int rollno;
+	String name;
+}
