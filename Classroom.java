ddd774ead31c5e38d01c964d2e0e52babd3c4a12
ashiq k p <muhammed.ashiq.k.p@lxisoft.com>
2019-08-17 09:38:45 +0530

    git exercise

---
+++ Classroom.java
@@ -0,0 +1,16 @@
+public class Classroom
+{
+
+	int std;
+	Teacher teacher1=new Teacher();
+	Teacher teacher2=new Teacher();
+	Student student1=new Student();
+	Student student2=new Student();
+
+	public void print()
+{
+	System.out.println(" standard :- "+std);
+}
+
+	 
+}
