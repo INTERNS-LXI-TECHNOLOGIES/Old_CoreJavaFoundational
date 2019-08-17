2d0ce8fcc0c3ac337984245c56d3c850c558ba64
amrithapm <amritha.p.m@lxisoft.com>
2019-08-17 12:43:29 -0700

    LI-76 school project

---
+++ school/Classroom.java
@@ -0,0 +1,17 @@
+public class Classroom
+{
+String div;
+Student student1=new Student();
+Student student2=new Student();
+Teacher teacher=new Teacher();
+
+public void details()
+{
+	System.out.println("class and division is "+div);
+	
+
+
+
+
+}
+}
---
+++ school/Student.java
@@ -0,0 +1,9 @@
+public class Student
+{
+int id;
+String stname;
+
+public void details()
+{
+System.out.println("roll number is "+id+" and name is "+stname);
+}}
