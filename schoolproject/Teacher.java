b1d9ac3f4f73a0d702953cf655f45d93c50b8bbf
megha-vt <megha.v.t@lxisoft.com>
2019-08-19 11:30:21 +0530

    L1-108 completed

--- schoolproject/Teacher.java
+++ schoolproject/Teacher.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
 public class Teacher
 {
+	Scanner sc=new Scanner(System.in);
   String name;
   String subject;
+
+
+  void setTeacherDetails()
+  {
+  	System.out.println("enter teacher name");
+	name =sc.nextLine();
+	System.out.println("enter subject ");
+	subject=sc.nextLine();
+
+
+  }
+
+  void getTeacherDetails()
+  {
+  	System.out.println("Teacher: " +name);
+  	System.out.println("Subject: " +subject);
+  }
+
 }
